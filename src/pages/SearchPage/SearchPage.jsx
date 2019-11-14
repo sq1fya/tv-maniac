62558f0eb446e7764652542efbbdea5b015e5d14
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2019-11-14 12:18:57 +0100

    use Axios to make the first request to API

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export const SearchPage = () => (
-  <div className="row">
-    <section className="col col-3">
-      <h2 className="h3">Bookmarks</h2>
-    </section>
+export class SearchPage extends Component {
+  state = {
+    clickCt: 0,
+    shows: [],
+  }
 
-    <section className="col">
-      <h1 className="h3">Search</h1>
+  handleClick = () => {
+    this.setState({ clickCt: this.state.clickCt + 1 })
+    const url = 'https://api.tvmaze.com/search/shows?q=batman'
+    axios.get(url).then(({ data }) => this.setState({ shows: data }))
+  }
 
-      <div className="input-group">
-        <input type="search" className="form-control" />
-        <div className="input-group-append">
-          <button className="btn btn-primary">search</button>
-        </div>
+  render() {
+    // console.count('Render')
+
+    return (
+      <div className="row">
+        <section className="col col-3">
+          <h2 className="h3">Bookmarks</h2>
+        </section>
+
+        <section className="col">
+          <h1 className="h3">Search {this.state.clickCt}</h1>
+
+          <pre>{JSON.stringify(this.state.shows, null, 2)}</pre>
+
+          <div className="input-group">
+            <input type="search" className="form-control" />
+            <div className="input-group-append">
+              <button onClick={this.handleClick} className="btn btn-primary">
+                search
+              </button>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
-  </div>
-)
+    )
+  }
+}
