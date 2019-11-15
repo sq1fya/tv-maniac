5e05f70a20f29d8f8101b3935fb2dfe54bd2276f
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2019-11-15 15:53:27 +0100

    SearchPage refactored to function component

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,66 +1,56 @@
-import React, { Component, MouseEvent } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Show, ShowResponse } from '../../models/tv.models'
-import { ShowTitle } from '../../components/ShowTitle/ShowTitle'
 import { Poster } from '../../components/Poster/Poster'
 import { SearchForm } from '../../components/SearchForm/SearchForm'
 import { Badge } from '../../components/Badge/Badge'
 
-type State = {
-  clickCt: number
-  shows: Show[]
-}
+export const SearchPage = () => {
+  const [shows, setShows] = useState<Show[]>([])
 
-export class SearchPage extends Component<{}, State> {
-  title: 'Hello'
+  useEffect(() => {
+    const daj = setInterval(() => console.log('daj'), 1000)
+    return () => {
+      clearInterval(daj)
+    }
+  })
 
-  state: State = {
-    clickCt: 0,
-    shows: [],
-  }
+  useEffect(() => {
+    search('batman')
+    return () => {
+      console.log('destroy!')
+    }
+  }, [])
 
-  componentDidMount(): void {
-    this.search('batman')
-  }
-
-  search = (query: string) => {
+  const search = (query: string) => {
     const url = `https://api.tvmaze.com/search/shows?q=${query}`
     axios
       .get<ShowResponse[]>(url)
-      .then(({ data }) =>
-        this.setState({ shows: data.map(({ show }) => show) }),
-      )
+      .then(({ data }) => setShows(data.map(({ show }) => show)))
   }
 
-  render() {
-    // console.count('Render')
+  return (
+    <div className="row">
+      <section className="col col-3">
+        <h2 className="h3">Bookmarks</h2>
+      </section>
 
-    return (
-      <div className="row">
-        <section className="col col-3">
-          <h2 className="h3">Bookmarks</h2>
-        </section>
+      <section className="col">
+        <h1 className="h3 d-flex">
+          Search <Badge value={shows.length} />
+        </h1>
 
-        <section className="col">
-          <h1 className="h3 d-flex">
-            Search <Badge value={this.state.shows.length} />
-          </h1>
+        <SearchForm initialQuery="batman" onSubmit={query => search(query)} />
 
-          <SearchForm
-            initialQuery="batman"
-            onSubmit={query => this.search(query)}
-          />
-
-          <div className="row">
-            {this.state.shows.map(show => (
-              <div className="col col-4" key={show.id}>
-                {/*<Poster image={show.image} name={show.name} />*/}
-                <Poster {...show} />
-              </div>
-            ))}
-          </div>
-        </section>
-      </div>
-    )
-  }
+        <div className="row">
+          {shows.map(show => (
+            <div className="col col-4" key={show.id}>
+              {/*<Poster image={show.image} name={show.name} />*/}
+              <Poster {...show} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  )
 }
